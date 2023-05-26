5e1fb72261bbea7c1bd34757d89e2af21c27a9b8
gabrielsoaresevt <gabrielsoaresevt@gmail.com>
2023-05-25 22:08:48 -0300

    :construction: add grid layout and initialization of components styles

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     <div className="App">
       <header className="header">
         <div className="container">
-          <img 
+          <img className="header__profile-photo"
             src="https://github.com/gabrielsoaresevt.png" 
             alt="Foto do Gabriel Soares Evangelista" />
           <p className="header__name">
@@ -19,36 +19,36 @@ export default function App() {
             </span>
           </p>
         </div>
-        <div className="container">
+        <div className="container side-menu">
           <ul className="header__list__social-media">
-            <li>
-              <a href="https://www.linkedin.com/in/gabriel-soares-evangelista">
+            <li className="header__list__social-media__item">
+              <a href="https://www.linkedin.com/in/gabriel-soares-evangelista" rel="noopener noreferrer">
                 <img 
                   src={linkedinIcon} 
                   alt="Ícone do LinkedIn" />
               </a>
             </li>
-            <li>
-              <a href="https://www.youtube.com/@gabrielsoares9405/featured">
+            <li className="header__list__social-media__item">
+              <a href="https://www.youtube.com/@gabrielsoares9405/featured" rel="noopener noreferrer">
                 <img 
                   src={youtubeIcon} 
                   alt="Ícone do LinkedIn" />
               </a>
             </li>
-            <li>
-              <a href="https://github.com/gabrielsoaresevt">
+            <li className="header__list__social-media__item"> 
+              <a href="https://github.com/gabrielsoaresevt" target="_blank" rel="noopener noreferrer">
                 <img 
                   src={githubIcon} 
                   alt="Ícone do LinkedIn" />
               </a>
             </li>
           </ul>
-          <ul>
-            <li><a href="#inicio">Início</a></li>
-            <li><a href="#portfolio">Portfólio</a></li>
-            <li><a href="#tech-stack">Tech Stack</a></li>
-            <li><a href="#contato">Contato</a></li>
-            <li><a href="#curriculo">Currículo</a></li>
+          <ul className="header__list__sections">
+            <li className="header__list__sections__item"><a href="#inicio">Início</a></li>
+            <li className="header__list__sections__item"><a href="#portfolio">Portfólio</a></li>
+            <li className="header__list__sections__item"><a href="#tech-stack">Tech Stack</a></li>
+            <li className="header__list__sections__item"><a href="#contato">Contato</a></li>
+            <li className="header__list__sections__item"><a href="#curriculo">Currículo</a></li>
           </ul>
         </div>
       </header>
@@ -57,12 +57,84 @@ export default function App() {
           <section className="main__about-me">
             <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
           </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
+          <section className="main__about-me">
+            <h2 id="#inicio" className="main__aboout-me__title">Sobre mim</h2>
+          </section>
         </div>
       </main>
-      <footer>
+      <footer className="footer">
         <div className="container">
-          <div className="hamburguer-menu"></div>
-          <img src={arrowUpIcon} alt="Seta para rolar a página para cima"></img>
+          <div className="footer__hamburguer-menu"></div>
+          <img className="footer__arrow-up" src={arrowUpIcon} alt="Seta para rolar a página para cima"></img>
         </div>
       </footer>
     </div>
